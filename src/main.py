1e152f34da6f3bca8cfadc10402635bd717aadd8
James Edington <james@ishygddt.xyz>
2022-02-12 00:14:45 -0600

    Nearly working, calling it a night

--- src/main.py
+++ src/main.py
@@ -1,201 +1,186 @@
 import chess
-from rng import RijndaelRng
 import zmq
+import bencodepy
+from cryptography.hazmat.primitives.asymmetric import x25519
+from cryptography.hazmat.primitives import serialization as _crypto_serialization
 import logging
-import time
+import json
+import hashlib
+import ed25519
 from sys import stdout
+from os import urandom
+from base64 import b64encode, b64decode
+from rng import RijndaelRng
 logging.basicConfig(level=logging.INFO, stream=stdout)
 
-def _main(my_color=chess.WHITE):
-	global board
-	board = chess.Board()
-	board.forget_pieces(lambda square: board.color_at(square) != my_color)
-	# White starts out with two pieces missing for debugging
-	if my_color == chess.WHITE:
-		board.remove_piece_at(chess.D2)
-		board.remove_piece_at(chess.E2)
-	if my_color == chess.BLACK:
-		their_turn()
-	while True:
-		if my_turn():
-			return "VICTORY"
-		if their_turn():
-			return "FAT LOSS"
+chess.NO_PIECE = -len(chess.PIECE_NAMES)
 
-def my_turn(board, my_color):
-	visibility = gain_vision(board, my_color)
-	print(board.unicode_fog(visibility))
-	move = chess.Move.from_uci(input("YUOR MOEV (in UCI FORMAT such as e2e4)\n> "))
-	target = board.piece_at(move.to_square)
-	assert target is None or target.color != my_color
-	board.push(move)
-	if target and target.piece_type == chess.KING:
-		return True
-	print(board.unicode_fog(visibility))
+CHESS_PORT = 64355
 
-def their_turn():
-	while True: _get_queried(black_view, chess.BLACK)
-
-
-def _is_visible(board, square, pov_color, vision_limit=99, self_vision=False):
-	if board.color_at(square) == pov_color:
-		# We have a square on this piece; exit early
-		return self_vision
-	for spotter_square in board.attackers(pov_color, square):
-		spotter = board.piece_at(spotter_square)
-		if spotter.piece_type not in chess.UNBLOCKABLE_PIECES or vision_limit < 1:
-			# Piece may be vision encumbered
-			if chess.square_distance(spotter_square, square) <= vision_limit:
-				# It could, in fact, see the distance
-				return True
-			else:
-				# Theoretically spotted, but outside of range
-				# skip this spotter and try the next
-				continue
+def _main(board, color):
+	winner = None
+	if color == chess.BLACK:
+		winner = their_turn(board)
+	while winner is None:
+		if board.turn == color:
+			winner = my_turn(board)
 		else:
-			# Knight with vision >= 1 -- unencumbrable
+			winner = their_turn(board)
+	if winner == color:
+		return 0
+	else:
+		return 1
+
+
+def my_turn(board):
+	revealed = probe_opponent(board)
+	logging.info("REVEALED: %s", revealed)
+	print(board.unicode())
+	move = chess.Move.from_uci(input('MOVE IN UCI FORMAT (e.g. e2e4)\n> '))
+	dest = move.to_square
+	target_piece = board.piece_at(dest)
+	if target_piece is None:
+		notify_capture(None)
+	else:
+		if target_piece.color != board.turn:
+			raise ValueError("Can't capture your own piece")
+		notify_capture(dest)
+		logging.info("CAPTURED: %s@%s", target_piece.symbol(), chess.SQUARE_NAMES[dest])
+	board.push(move)
+	forget_player(board, board.turn)
+
+
+def their_turn(board):
+	forget_player(board, board.turn)
+	queries = respond_to_probe(board)
+	captured_square = rcv_notify_capture()
+	if captured_square is None:
+		captured_piece = None
+		console.info("NO CAPTURE BY OPPONENT THIS TURN")
+	else:
+		captured_piece = board.piece_at(captured_square)
+		board.push(chess.Move(captured_square, captured_square, drop=chess.NO_PIECE))
+		console.info("OPPONENT CAPTURED: %s@%s", captured_piece.symbol(), chess.SQUARE_NAMES[captured_square])
+	if captured_piece.piece_type == chess.KING:
+		return not board.turn
+
+
+def check_vision(board, square, max_vision=99, self_vision=False):
+	piece = board.piece_at(square)
+	if piece and piece.color == board.turn:
+		return self_vision
+	for attacker_square in board.attackers(board.turn, square):
+		attacker = board.piece_at(attacker_square)
+		if attacker.piece_type != chess.KNIGHT or max_vision < 1:
+			if chess.square_distance(attacker_square, square) <= max_vision:
+				return True
+		else:
 			return True
-	# All candidate spotters -- if there were any -- were disqualified
-	# Square is clearly not visible with current boardstate and vision
 	return False
 
-def _calc_visibility(board, pov_color, vision_limit=99, self_vision=False):
-	visibility = chess.SquareSet(chess.BB_EMPTY)
+
+def calc_vision(board, max_vision=99, self_vision=False):
+	vision = chess.SquareSet()
 	for square in chess.SQUARES:
-		if _is_visible(board, square, pov_color, vision_limit, self_vision):
-			visibility.add(square)
-	return visibility
+		if square in vision:
+			continue
+		if check_vision(board, square, max_vision, self_vision):
+			vision.add(square)
+	return vision
 
 
-def _pickle_piecemap(piecemap):
-	return dict(((square, piece.symbol()) for square, piece in piecemap.items()))
 
-def _unpickle_piecemap(dict_piecemap):
-	return dict(((int(square), chess.Piece.from_symbol(symbol)) for square, symbol in dict_piecemap.items()))
-
-def _pickle_squareset(squareset):
-	return list(squareset)
-
-def _unpickle_squareset(list_squareset):
-	return chess.SquareSet(list_squareset)
-
-
-def _query_opponent(squares, color):
-	ctx = zmq.Context()
-	
-	socket = ctx.socket(zmq.PAIR)
-	socket.connect('tcp://localhost:%i' % (64355 + color))
-	socket.send_json(_pickle_squareset(squares))
-	new_pieces = _unpickle_piecemap(socket.recv_json())
-	return new_pieces
-
-
-def _get_queried(board, color):
+def probe_opponent(board):
+	_Board = chess.BaseBoard if type(board) == chess.Board else type(board)
 	ctx = zmq.Context()
 	socket = ctx.socket(zmq.PAIR)
-	socket.bind('tcp://*:%i' % (64355 + color))
-	previous_squares = None
-	squares = chess.SquareSet(socket.recv_json())
-	while squares != previous_squares:
-		visible_pieces = board.piece_map(mask=int(squares))
-		socket.send_json(_pickle_piecemap(visible_pieces))
-		squares, previous_squares = chess.SquareSet(socket.recv_json()), squares
-	return squares
-
-
-def gain_vision(board, self_color, *, queryfunc=_query_opponent, runs=range(1, 8), **kwargs):
-	visibility_generator = _gain_vision(board, self_color, runs=runs, **kwargs)
-	visibility = next(visibility_generator)
-	while True:
-		try:
-			new_pieces = queryfunc(visibility, not self_color)
-			visibility = visibility_generator.send(new_pieces)
-		except StopIteration:
-			break
-	return visibility
-
-def _gain_vision(board, self_color, *, runs=range(1, 8), **kwargs):
-	for i in runs:
-		print("STARTING ROUND %i" % i)
-		new_pieces = (yield _calc_visibility(board, self_color, vision_limit=i, **kwargs))
-		time.sleep(1)
-		for square, piece in new_pieces.items():
-			print("ADDING PIECE %s AT %s" % (repr(piece), repr(square)))
-			board.set_piece_at(square, piece)
-
-#def _query_opponent(squares):
-#	global _opponent
-#	new_pieces = dict()
-#	for square in squares:
-#		new_piece = _opponent.piece_at(square)
-#		if new_piece is not None:
-#			new_pieces[square] = new_piece
-#	return new_pieces
-
-
-chess.UNBLOCKABLE_PIECES = {chess.KNIGHT}
-
-# TODO: class DarkBoard(BaseBoard)
-
-def _forget_pieces(board, predicate):
-	for square in filter(predicate, chess.SQUARES):
-		board.remove_piece_at(square)
-
-chess.BaseBoard.forget_pieces = _forget_pieces
-
-def _str_fog(board, visibility, hidden_symbol="?"):
-	builder = []
-	visibility = chess.SquareSet(visibility)
-	for square in chess.SQUARES_180:
-		piece = board.piece_at(square)
-		if piece:
-			builder.append(piece.symbol())
-		else:
-			builder.append("." if (square in visibility) else hidden_symbol)
-		if chess.BB_SQUARES[square] & chess.BB_FILE_H:
-			if square != chess.H1:
-				builder.append("\n")
-		else:
-			builder.append(" ")
-	return "".join(builder)
-
-chess.BaseBoard.str_fog = _str_fog
-
-def _unicode_fog(board, visibility, *, hidden_square="\u2047", invert_color=False, borders=False, empty_square="\u2B58"):
-	builder = []
-	visibility = chess.SquareSet(visibility)
-	for rank_index in range(7, -1, -1):
-		if borders:
-			builder.append("  ")
-			builder.append("-" * 17)
-			builder.append("\n")
-			builder.append(RANK_NAMES[rank_index])
-			builder.append(" ")
-		for file_index in range(8):
-			square = chess.square(file_index, rank_index)
-			if borders:
-				builder.append("|")
-			elif file_index > 0:
-				builder.append(" ")
-			piece = board.piece_at(square)
+	socket.connect('tcp://localhost:%u' % (CHESS_PORT + board.halfmove_clock))
+	for max_vision in range(1, 8):
+		seed = urandom(32) # TODO allow Bob to ensure randomness
+		logging.info('CHOSE SEED: %s', b64encode(seed, b'-_').decode())
+		hseed = h(seed)
+		logging.info('SEED COMMITMENT: %s', b64encode(hseed, b'-_').decode())
+		vision = calc_vision(board, max_vision)
+		pkeys = gen_fake_pubkeys(seed)
+		keys = []
+		for square in vision:
+			sk, pk = gen_real_keypair()
+			keys.append(sk)
+			pkeys[square] = pk
+		logging.info('CLAIMING VISION %u: %s', max_vision, json.dumps(list(vision)))
+		socket.send_serialized([hseed, pkeys], _serialize)
+		payload = socket.recv_serialized(_deserialize)
+		piece_map = dict()
+		for square, sk in zip(vision, keys):
+			piece_data = pk_decrypt(sk, payload[square])
+			piece = chess.Piece.fromsymbol(piece_data.decode()) if piece_data != b'\x00' else None
+			piece_map[square] = piece
 			if piece:
-				builder.append(piece.unicode_symbol(invert_color=invert_color))
-			else:
-				builder.append(empty_square if (square in visibility) else hidden_square)
-		if borders:
-			builder.append("|")
-		if borders or rank_index > 0:
-			builder.append("\n")
-	if borders:
-		builder.append("  ")
-		builder.append("-" * 17)
-		builder.append("\n")
-		builder.append("   a b c d e f g h")
-	return "".join(builder)
+				_Board.set_piece_at(square, piece)
+		logging.info('GOT VISION: %s', json.dumps(dict(((chess.SQUARE_NAMES[k], v.symbol() if v else None) for k, v in piece_map.items()))))
 
-chess.BaseBoard.unicode_fog = _unicode_fog
+
+def respond_to_probe(board):
+	ctx = zmq.Context()
+	socket = ctx.socket(zmq.PAIR)
+	socket.bind('tcp://*:%u' % (CHESS_PORT + board.halfmove_clock))
+	for _ in range(1, 8):
+		hseed, pkeys = socket.recv_serialized(_deserialize)
+		logging.info('GOT SEED COMMITMENT: %s', b64encode(hseed, b'-_').decode())
+		logging.info('GOT QUERY: %s', json.dumps(list(b64encode(pk, b'-_').decode() for pk in pkeys)))
+		payload = []
+		for square, pk in zip(chess.SQUARES, pkeys):
+			# note: you should forget opponent pieces BEFORE running this function
+			piece = board.piece_at(square)
+			piece_data = piece.symbol().encode() if piece else b'\x00'
+			encrypted_piece_data = pk_encrypt(pk, piece_data)
+			payload.append(encrypted_piece_data)
+		socket.send_serialized(payload, _serialize)
+
+
+def _serialize(m):
+	return [bencodepy.encode(m)]
+
+def _deserialize(b):
+	return bencodepy.decode(bytes().join(b))
+
+
+def h(m):
+	return hashlib.sha256(m).digest()
+
+def gen_fake_pubkeys(seed, m=32, n=64):
+	key = h(seed)
+	r = RijndaelRng(seed)
+	payload = []
+	while len(payload) < n:
+		pk = urandom(m)
+		try:
+			ed25519.decodepoint(pk)
+		except ValueError:
+			pass
+		else:
+			payload.append(pk)
+	return payload
+
+def gen_real_keypair(m=32):
+	sk = urandom(m)
+	pk = publickey(sk)
+	return sk, pk
+
+def publickey(sk):
+	return x25519.X25519PrivateKey.from_private_bytes(sk).public_key().public_bytes(encoding=_crypto_serialization.Encoding.Raw, format=_crypto_serialization.PublicFormat.Raw)
+
+
+def forget_player(board, color):
+	_Board = chess.BaseBoard if type(board) == chess.Board else type(board)
+	for square in chess.SQUARES:
+		piece = board.piece_at(square)
+		if piece and piece.color == color:
+			_Board.remove_piece_at(board, square)
+
 
 if __name__ == '__main__':
-	import sys, os
-	color = {"white": chess.WHITE, "black": chess.BLACK}[os.environ.get("chesscolor", "white")]
-	sys.exit(_main(color))
+	import os, sys
+	board = chess.Board()
+	color = {'white': chess.WHITE, 'black': chess.BLACK}[os.environ.get('chesscolor', 'white')]
+	_main(board, color)
