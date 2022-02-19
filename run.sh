3f4d24c07e85a62e64e50b1f4ef08f2ba5bffa97
James Edington <james@ishygddt.xyz>
2022-02-19 17:26:04 -0600

    EZier

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 cd "${0%/*}"
+python3 -m venv env
+. env/bin/activate
+pip install -r requirements.txt
 cd src
-python3 -m venv ../env
-. ../env/bin/activate
 python -m DarkChess "$@"
